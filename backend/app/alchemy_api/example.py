f9d8f775d7bf415cef511073f931335c469bfe70
Eric Ahn <wchill1337@gmail.com>
2015-09-12 04:39:29 -0400

    fix api example

--- backend/app/alchemy_api/example.py
+++ backend/app/alchemy_api/example.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
+from alchemyapi import AlchemyAPI
+import json
 
 
-#demo_text = 'Five cornfields were randomly selected in each county and within each field, 100 consecutive whorl-stage plants were sampled for any signs of whorl feeding or the presence of European corn borer larvae. An action site was sampled for European corn borer moths near each field by making 100 sweeps with a standard insect sweep net. We characterized action sites as dense stands of tall grasses in roadside ditches or nearby waterways. Within these action sites moths congregate and mating occurs.'
+demo_text = 'Five cornfields were randomly selected in each county and within each field, 100 consecutive whorl-stage plants were sampled for any signs of whorl feeding or the presence of European corn borer larvae. An action site was sampled for European corn borer moths near each field by making 100 sweeps with a standard insect sweep net. We characterized action sites as dense stands of tall grasses in roadside ditches or nearby waterways. Within these action sites moths congregate and mating occurs.'
 demo_url = 'http://www.refinery29.com/2015/09/93665/apple-keynote-iphone-6s-6s-plus#.hlxatb:YdSn'
 demo_html = '<html><head><title>HTML | PARSING</title></head><body><h1>PARSE HERE</h1><p>PARSE HERE</p></body></html>'
 image_url = 'http://s1.r29static.com//bin/entry/1ae/x/1473886/image.png'
