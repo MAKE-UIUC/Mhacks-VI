573c5a0ae8a24f994f4d7818bfed0c425513a930
Abhishek Modi <abhishek.modi95@gmail.com>
2015-09-12 11:39:37 -0400

    Shifted to Objective C

---
+++ iOS/streampoint/streampoint/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  streampoint
+//
+//  Created by Abhishek Modi on 9/12/15.
+//  Copyright (c) 2015 MAKE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOS/streampoint/streampoint/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  streampoint
+//
+//  Created by Abhishek Modi on 9/12/15.
+//  Copyright (c) 2015 MAKE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
