8b6e2c6b9b5eed36011686f20d31d9ed57126f14
Abhishek Modi <abhishek.modi95@gmail.com>
2015-09-13 11:49:13 -0400

    Jank UI Done

---
+++ iOS/streampoint/streampoint/PostRequestHelp.m
@@ -0,0 +1,13 @@
+//
+//  PostRequestHelp.m
+//  streampoint
+//
+//  Created by Abhishek Modi on 9/13/15.
+//  Copyright (c) 2015 MAKE. All rights reserved.
+//
+
+#import "PostRequestHelp.h"
+
+@implementation PostRequestHelp
+
+@end
--- iOS/streampoint/streampoint/ViewController.h
+++ iOS/streampoint/streampoint/ViewController.h
@@ -10,14 +10,21 @@
 #import <SpeechKit/SpeechKit.h>
 #import "AppDelegate.h"
 #import "SocketSend.h"
+#import <Foundation/Foundation.h>
+
+@import Foundation;
 
 @interface ViewController : UIViewController <SpeechKitDelegate, SKRecognizerDelegate>
 @property (strong, nonatomic) SKRecognizer* voiceSearch;
 @property (strong, nonatomic) AppDelegate *appDelegate;
 @property (weak, nonatomic) IBOutlet UIButton *RecordButton;
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData*)data;
 - (IBAction)NextSlide:(id)sender;
 - (IBAction)Record_Down:(id)sender;
 - (IBAction)Record_UpIn:(id)sender;
+- (void)createRequest:(NSString*)url singleval:(NSString*)val singlearg:(NSString*)arg;
+@property (weak, nonatomic) IBOutlet UIImageView *ImageDisp;
+@property (weak, nonatomic) IBOutlet UIButton *AskWolfram;
 
 @end
 
